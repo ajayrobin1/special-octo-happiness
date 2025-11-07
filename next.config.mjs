13d72dc384e7c1061b1019d3928d21480aa4cde4
ajayrobin1 <ajayrobin10@gmail.com>
2025-11-07 22:53:52 +0530

    auth function

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      webpack5: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  };
-  },
        images: {
         domains: ['res.cloudinary.com'],
     },
